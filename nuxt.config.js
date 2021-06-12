cccd6c6b6846f90f87e6facd83ed1546924f9010
Admin-web-user <superuser00021@gmail.com>
2021-06-13 00:42:54 +0600

    removed .env and .env.example

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,8 +41,7 @@ export default {
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
-    '@nuxtjs/dotenv'
+    '@nuxtjs/vuetify'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
